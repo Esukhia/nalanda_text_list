dca0f82d47ca0e0d3b765a66ef7ee75c37c3a956
kaldan007 <kaldantenzin@gmail>
2022-03-22 10:24:48 +0530

    batch 4 hfml ready

--- serilise_hfml.py
+++ serilise_hfml.py
@@ -38,12 +38,13 @@ def get_text_list(list_path):
     return text_list
 
 if __name__ == "__main__":
-    # parma = "derge_google_pedurma"
-    # opf_path = Path('./opfs/12d32eb31c1a4cc59741cda99ebc7211.opf')
+    parma = "derge_google_pedurma"
+    opf_path = Path('./opfs/12d32eb31c1a4cc59741cda99ebc7211.opf')
     
-    parma = "namsel_pedurma"
-    opf_path = Path('./opfs/187ed94f85154ea5b1ac374a651e1770.opf')
+    # parma = "namsel_pedurma"
+    # opf_path = Path('./opfs/187ed94f85154ea5b1ac374a651e1770.opf')
     text_ids = get_text_list(Path('./narthang_text.txt'))
+    text_ids = ['Q5221', 'Q5700', 'Q5706', 'Q5811', 'Q5814']
     index = from_yaml((opf_path / "index.yml"))
     for text_id in text_ids:
         hfmls = get_hfml_text(opf_path, text_id, index=index)
